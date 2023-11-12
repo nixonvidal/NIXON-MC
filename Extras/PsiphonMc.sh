5f803c0edf1a0df4225869bea5ceaf9d1012e145
nixonvidal <147317708+nixonvidal@users.noreply.github.com>
2023-11-12 15:47:17 -0500

    Update PsiphonMc.sh

--- Extras/PsiphonMc.sh
+++ Extras/PsiphonMc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 instalar_psiphond() {
     echo "Instalando PsiphonD..."
-    wget https://github.com/Psiphon-Labs/psiphon-tunnel-core-binaries/raw/master/psiphond/psiphond &
+    wget https://github.com/Psiphon-Labs/psiphon-tunnel-core-binaries/raw/master/psiphond/psiphond &>/dev/null
     chmod 777 psiphond
     ip=$(wget -qO- ifconfig.me)
     echo "————————————————————————————————————————————————————"
